dbb035fd01d20bb05dd2507da5d31e0fb17cd6cf
Hong Minhee <hong.minhee@gmail.com>
2021-12-01 23:27:01 +0900

    IOffloadOptions interface & OffloadOptions class

---
+++ Bencodex/IOffloadOptions.cs
@@ -0,0 +1,28 @@
+﻿using Bencodex.Types;
+
+namespace Bencodex
+{
+    /// <summary>
+    /// Options specifying how to offload heavy values of lists and dictionaries.
+    /// </summary>
+    public interface IOffloadOptions
+    {
+        /// <summary>
+        /// Determines whether a given <paramref name="indirectValue"/> should be embedded or
+        /// offloaded.
+        /// </summary>
+        /// <param name="indirectValue">A value to determine whether to embed or offload.</param>
+        /// <returns><see langword="true"/> if <paramref name="indirectValue"/> should be embedded;
+        /// <see langword="false"/> if it should be offloaded.</returns>
+        public bool Embeds(in IndirectValue indirectValue);
+
+        /// <summary>
+        /// Stores an offloaded value in a separate place.
+        /// </summary>
+        /// <param name="indirectValue">An offloaded value.</param>
+        /// <param name="loader">An optional loader used for loading
+        /// the <paramref name="indirectValue"/>.  This can be <see langword="null"/> if
+        /// the <paramref name="indirectValue"/> is already loaded.</param>
+        public void Offload(in IndirectValue indirectValue, IndirectValue.Loader? loader);
+    }
+}
--- CHANGES.md
+++ CHANGES.md
@@ -20,8 +20,8 @@ To be released.
         constructor.  [[#52]]
      -  Added `Dictionary(IEnumerable<KeyValuePair<IKey, IndirectValue>>,
         IndirectValue.Loader)` constructor.  [[#52]]
-     -  Added `List.EnumerateIndirectValues()` method.  [[#53]]
-     -  Added `Dictionary.EnumerateIndirectValues()` method.  [[#53]]
+     -  Added `Bencodex.IOffloadOptions` interface. [[#55]]
+     -  Added `Bencodex.OffloadOptions` sealed class. [[#55]]
  -  Bencodex values now have their unique fingerprints:  [[#50]]
      -  Added `Bencodex.Types.Fingerprint` readonly struct.
      -  Added `Bencodex.Misc.FingerprintComparer` class.
@@ -61,6 +61,7 @@ To be released.
 [#52]: https://github.com/planetarium/bencodex.net/pull/52
 [#53]: https://github.com/planetarium/bencodex.net/pull/53
 [#54]: https://github.com/planetarium/bencodex.net/pull/54
+[#55]: https://github.com/planetarium/bencodex.net/pull/55
 
 
 Version 0.3.0
